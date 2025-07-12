0269f0e674dc970a3bf49af27c6b7784c5a385d8
vemurikethan <kethan.vemuri@infineon.com>
2025-07-13 00:09:19 +0530

    fix: update router history configuration to remove unnecessary base path

--- src/router/index.js
+++ src/router/index.js
@@ -29,9 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(
-    `${import.meta.env.BASE_URL}afonso-management-llc/`
-  ),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/afonso-management-llc/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
